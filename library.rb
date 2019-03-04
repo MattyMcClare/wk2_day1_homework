dfc54c8ffecf058d30833e9a22d045a4fd9d3635
Mateusz <matstanczak@gmail.com>
2019-03-04 21:34:47 +0000

    homework libraries new_book

--- library.rb
+++ library.rb
@@ -1,10 +1,38 @@
-class Library{
-    title: "lord_of_the_rings",
-    rental_details: {
-     student_name: "Jeff",
-     date: "01/12/16"
+class Library
+  attr_accessor :books
+  def initialize(books)
+    @books = books
+  end
+
+  def get_book_details(title)
+    for book in @books
+      if (title == book[:title])
+        return book
+      end
+    end
+  end
+
+  def get_book_rental_details(title)
+    for book in @books
+      if (title == book[:title])
+        return book[:rental_details]
+      end
+    end
+  end
+
+  def add_book_by_title(title)
+    new_book = {
+      title: title,
+      rental_details: {
+       student_name: "",
+       date: ""
+      }
     }
-  }
-  
+    for book in @books
+      if (title != book[:title])
+      return books.push(new_book)
+      end
+    end
+  end
 
 end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -3,5 +3,49 @@ require('minitest/rg')
 require_relative('../library')
 
 class TestLibrary < MiniTest::Test
-  def 
+
+  def setup
+    @books = [
+      {
+        title: "Harry Potter:The Philosopher's Stone",
+        rental_details: {
+         student_name: "Matt",
+         date: "1997"
+        }
+      },
+      {
+        title: "Harry Potter:The Chamber of Secrets",
+        rental_details: {
+         student_name: "Bob",
+         date: "1998"
+        }
+      }
+    ]
+
+
+  end
+
+  def test_get_books
+    library = Library.new(@books)
+    assert_equal("Harry Potter:The Philosopher's Stone", library.books[0][:title])
+  end
+
+  def test_get_book_details
+    library = Library.new(@books)
+    book = library.get_book_details("Harry Potter:The Philosopher's Stone")
+    assert_equal(book[:title], "Harry Potter:The Philosopher's Stone")
+  end
+
+  def test_get_book_rental_details
+    library = Library.new(@books)
+    book = library.get_book_rental_details("Harry Potter:The Philosopher's Stone")
+    assert_equal(book[:date], "1997")
+  end
+
+  def test_add_book_by_title
+    library = Library.new(@books)
+    book = library.add_book_by_title("Harry Potter:The Prisoner of Azkaban")
+    assert_equal(3, @books.length)
+  end
+
 end
